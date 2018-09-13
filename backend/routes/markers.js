261c551335956318233f34da37e720e71800b624
Adel Kovacs <kovacsadel12@gmail.com>
2018-09-13 21:15:00 +0200

    id typo

--- backend/routes/markers.js
+++ backend/routes/markers.js
@@ -37,7 +37,7 @@ router.get("/:weddingName", (req, res, next) => {
 );
 
 router.patch("/:weddingName", (req, res, next) => {
-    Wedding.update({"name": req.params.weddingName, "markers._id": req.body.id},
+    Wedding.update({"name": req.params.weddingName, "markers._id": req.body._id},
         {
             $set: {
                 "markers.$.lat": req.body.lat, "markers.$.lng": req.body.lng
